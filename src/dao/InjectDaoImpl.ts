a65a7ce75ec48f2f708625aed0be5a825c2a5dd9
gegan <2319649532@qq.com>
2019-10-16 18:02:47 +0800

    use pagnation success

--- src/dao/InjectDaoImpl.ts
+++ src/dao/InjectDaoImpl.ts
@@ -80,8 +80,8 @@ class InjectDaoImpl implements InjectDao{
     console.log('客户端传入的分页数据',cursor);
     return new Promise((resolve,reject)=>{
         //selete * from testtable limit 2 offset 1;
-        let sql=`SELECT * FROM inject_test limit ${limit} offset ${cursor-1}`;
-        dbConnection.query(sql,(err,result)=>{
+        let sql1=`SELECT * FROM inject_test limit ${limit} offset ${limit*(cursor-1)}`;
+        dbConnection.query(sql1,(err,result)=>{
             if(!err){
                 //返回表格中绑定的列名
                 let ijTree=[
@@ -129,13 +129,22 @@ class InjectDaoImpl implements InjectDao{
                       "prop": "infoUrl",
                       "label": "infoUrl"
                     }
-                  ]
-                //将查到的数据重新包装
-                let resultVo={
-                  ijArr:result,
-                  ijTree
+                  ];
+                  let resultVo={
+                    ijArr:result,
+                    ijTree
                 }
-                resolve(new Message({msg:'ok',data:resultVo,stCode:200}))
+                  //查询总条数
+                  let sql2="SELECT COUNT(*) AS count FROM inject_test;"
+                  dbConnection.query(sql2,(err,result2)=>{
+                      if(!err){
+                         (resultVo as any).count=result2[0].count,
+                          //将查到的数据重新包装
+                         resolve(new Message({msg:'ok',data:resultVo,stCode:200}))
+                      }else{
+                        resolve(new Message({msg:'ok',data:resultVo,stCode:200}))
+                      }
+                  })
                 //  resolve(result);
                 // resolve(new Message({msg:'ok',data:result,stCode:200}));
             }else{
